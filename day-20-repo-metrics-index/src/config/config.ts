0f57aab8d5289a8a57c96615d21d7c922d137e4d
BonnieProsper <bonniep.mcconnell@gmail.com>
2026-02-20 14:54:34 +1300

    update config for git, file options

--- day-20-repo-metrics-index/src/config/config.ts
+++ day-20-repo-metrics-index/src/config/config.ts
@@ -3,9 +3,15 @@ export interface RepoIndexConfig {
   churnWindowDays: number
   minChurnThreshold?: number
   includeBinaryAnalysis: boolean
+  includeExtensions: string[] | null
+  parallelism: number
+  enableGit: boolean
 }
 
 export const DEFAULT_CONFIG: Omit<RepoIndexConfig, "rootPath"> = {
   churnWindowDays: 90,
-  includeBinaryAnalysis: true
+  includeBinaryAnalysis: true,
+  includeExtensions: null, // null = include all
+  parallelism: 4,
+  enableGit: true
 }
